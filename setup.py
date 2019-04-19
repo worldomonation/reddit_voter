9cc54af0d9762b21f687560aefde78a1f1dff235
Edwin Gao <egao@outlook.com>
2019-04-19 12:14:35 -0700

    Added setup.py with relevant information, automated installation of dependencies, etc.

---
+++ setup.py
@@ -0,0 +1,41 @@
+import os
+
+from setuptools import setup
+
+MAJOR_VERSION = 0
+MINOR_VERSION = 0
+REVISION = 2
+
+PACKAGE_VERSION = '.'.join([
+    str(item) for item in [MAJOR_VERSION, MINOR_VERSION, REVISION]
+])
+DESCRIPTION = '''A mass-upvote/downvote client for Reddit.'''
+with open(os.path.join(os.path.dirname(
+        os.path.abspath(__file__)), 'README.md')) as f:
+    README = f.read()
+
+DEPENDENCIES = [
+    'flake8',
+    'praw',
+    'progressbar'
+]
+
+setup(
+    name='reddit_voter',
+    version=PACKAGE_VERSION,
+    description=DESCRIPTION,
+    long_description=README,
+    keywords='reddit',
+    author='Edwin Gao',
+    author_email='egao@outlook.com',
+    url='https://github.com/worldomonation/reddit_voter',
+    license='',
+    packages=['reddit_voter'],
+    include_package_data=True,
+    install_requires=DEPENDENCIES,
+    entry_points="""
+    # -*- Entry points: -*-
+    [console_scripts]
+    voter = reddit_voter.reddit_voter:main
+    """,
+)
